5b6032070114b6f8ba081c853ad62f294628fc6d
Piret Saar <piret.saar@gmail.com>
2024-08-17 16:10:52 +0200

    File context is now using a backup in case the context has become corrupted.

--- autor/framework/file_context.py
+++ autor/framework/file_context.py
@@ -14,6 +14,7 @@
 import json
 import logging
 import traceback
+from typing import Dict
 
 from autor.framework.debug_config import DebugConfig
 from autor.framework.extension_exception import AutorExtensionException
@@ -27,41 +28,71 @@ class FileContext(RemoteContext):
     # ------------------
     # A string constant to use as the name of file which is going to be the file context.
     _FILENAME = "file_context.json"
+    _BACKUP_FILENAME = "file_context_backup.json"
     # pylint: disable-next=redefined-builtin
     def sync(self, id: str, context: dict) -> None:
         file_content = {}
+
+
+
+        # Read the remote context
         try:
             with open(self._FILENAME, "r", encoding="utf8") as file_context:
                 file_content = json.load(file_context)
                 remote_context = file_content.get(id, {})  # File may contain more than one context
         except FileNotFoundError:
             remote_context = {}
+
         except Exception as exception:
-            raise AutorExtensionException(
-                f"Failed to open file context for syncing. Exception: {exception.args}"
-            ) from exception
+            logging.warning(f"Failed to open context file:{self._FILENAME} for syncing. Exception: {exception.args}")
+            logging.warning(f"Trying to use backup: {self._BACKUP_FILENAME}")
+            try:
+                with open(self._BACKUP_FILENAME, "r", encoding="utf8") as file_context:
+                    file_content = json.load(file_context)
+                    logging.warning(f"Successfully read: {self._BACKUP_FILENAME}")
+                    remote_context = file_content.get(id, {})  # File may contain more than one context
+            except FileNotFoundError:
+                logging.error(f"Could not find the backup file: {self._BACKUP_FILENAME} -> raising an exception.")
+                raise AutorExtensionException(
+                    f"Failed to open file context: {self._FILENAME} and context backup: {self._BACKUP_FILENAME} does not exist .Exception: {exception.args}"
+                ) from exception
+
+            except Exception as exception:
+                raise AutorExtensionException(
+                    f"Failed to open file context: {self._FILENAME} and {self._BACKUP_FILENAME} for syncing. Exception: {exception.args}"
+                ) from exception
+
 
         if DebugConfig.trace_context:
             logging.info(f"ID:     {id}")
             Util.print_dict(context, "local context before sync", 'info')
             Util.print_dict(remote_context, "remote context before sync", 'info')
 
+
+        # Update the context of the remote flow with the local flow context
         remote_context.update(context)
+
+        # Update the local flow context with the remote flow context
         context.update(remote_context)
 
         if DebugConfig.trace_context:
             logging.info(f"ID:     {id}")
             Util.print_dict(context, "context after sync", 'info')
 
+        # Update the file context with the updated flow context
         file_content.update({id: remote_context})
 
+        self._write(file_content, self._BACKUP_FILENAME)
+        self._write(file_content, self._FILENAME)
+
+
+
+    def _write(self, file_content:Dict, file_name:str):
         try:
-            with open(self._FILENAME, "w", encoding="utf8") as file_context:
+            with open(file_name, "w", encoding="utf8") as file_context:
                 json.dump(file_content, file_context, indent=2)
         except Exception as exception:
-            # logging.error(f"Failed to open file context for syncing. Exception: {e.args}")
             raise AutorExtensionException(
-                f"Failed to open file context for syncing. Exception: {exception.args}"
+                f"Failed to open file context: {file_name} for writing. Exception: {exception.args}"
             ) from exception
-
 
